f35bb6ab196b2c26d6436bc9736fb6e66f9c53d5
Arnaud Duhé <anro010@hotmail.com>
2020-06-03 17:04:06 +0100

    squeleton done

---
+++ src/Components/Admin/AllWorkshopsSubComponents/WorkshopDetails.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const WorkshopDetails = () => {
+
+        return (
+            <div>
+                    <div>
+                        <p>Title: {}</p>
+                        <p>Status: {}</p>
+                        <p>Date: {}</p>
+                        <p>Description: {}</p>
+                        <p>Room: {}</p>
+                    </div>
+            </div>
+        );
+}
+
+
+export default WorkshopDetails;
