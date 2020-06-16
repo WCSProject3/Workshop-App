1adda6997a165c32e9e10176574258f7b29af7a2
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-06-16 15:10:47 +0100

    just missing some css on Edit Profile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Profile from './Components/SharedComponents/Profile';
-import Login from './Components/SharedComponents/Login'
-import SignUp from './Components/SharedComponents/SignUp'
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
     </div>
   );
 }
