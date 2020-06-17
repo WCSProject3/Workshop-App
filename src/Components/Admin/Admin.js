a450fe09f7277f0b2f0555d22868f9825f5b5871
Arnaud Duhé <anro010@hotmail.com>
2020-06-17 10:39:22 +0100

    notifications context created and set up into admin.js

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import NewWorkshop from "./NewWorkshop";
+import NotificationContextProvider from "../../Context/NotificationContext";
 import WorkshopContextProvider from "../../Context/WorkshopContext";
 import RoomContextProvider from "../../Context/RoomContext";
 import AttendeeContextProvider from "../../Context/AttendeeContext";
 import WorkshopAttendees from "./WorkshopAttendees";
+import AllNotifications from "./AllNotifications";
 import AllWorkshops from "./AllWorkshops";
 import NavBar from "../SharedComponents/NavBar";
 
 const Admin = () => {
   return (
     <BrowserRouter>
-    <div>
-      <AttendeeContextProvider>
-        <WorkshopContextProvider>
-          <RoomContextProvider>
-            <NavBar />
-            <NewWorkshop />
-            <WorkshopAttendees />
-            <AllWorkshops />
-          </RoomContextProvider>
-        </WorkshopContextProvider>
-      </AttendeeContextProvider>
-    </div>
-   </BrowserRouter>
+      <div>
+        <NotificationContextProvider>
+          <AttendeeContextProvider>
+            <WorkshopContextProvider>
+              <RoomContextProvider>
+                <NavBar />
+                <NewWorkshop />
+                <WorkshopAttendees />
+                <AllNotifications />
+                <AllWorkshops />
+              </RoomContextProvider>
+            </WorkshopContextProvider>
+          </AttendeeContextProvider>
+        </NotificationContextProvider>
+      </div>
+    </BrowserRouter>
   );
 };
 
