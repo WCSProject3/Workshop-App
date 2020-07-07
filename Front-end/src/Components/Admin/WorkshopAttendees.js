2db2c0df20cc85b308ff0534fca938ab98552866
François ARBOGAST <arbogast.francois@gmail.com>
2020-07-07 16:38:11 +0100

    export end

--- Front-end/src/Components/Admin/WorkshopAttendees.js
+++ Front-end/src/Components/Admin/WorkshopAttendees.js
@@ -1,49 +1,62 @@
-import React, { useState, useContext, useEffect } from 'react';
-import AttendeesList from './WorkshopAttendeesSubComponents/AttendeesList';
-import WorkshopInfo from './WorkshopAttendeesSubComponents/WorkshopInfo';
-import './WorkshopAttendees.scss';
-import { WorkshopContext } from '../../Context/WorkshopContext';
-import ModalForm from './Modals/ModalForm';
-import axios from 'axios';
+import React, { useState, useContext, useEffect } from "react";
+import AttendeesList from "./WorkshopAttendeesSubComponents/AttendeesList";
+import WorkshopInfo from "./WorkshopAttendeesSubComponents/WorkshopInfo";
+import "./WorkshopAttendees.scss";
+import { WorkshopContext } from "../../Context/WorkshopContext";
+import ModalForm from "./Modals/ModalForm";
+import axios from "axios";
 
+const WorkshopAttendees = (props) => {
+  const workshopId = props.match.params.id;
 
-const WorkshopAttendees = ( props ) => {
+  const { workshop, getWorkshop, getAttendees, attendees } = useContext(
+    WorkshopContext
+  );
+  const [displayModal, setDisplayModal] = useState(false);
+  const [workshopInEdit, setWorkshopInEdit] = useState([]);
+  const [modal, setModal] = useState("");
 
-    const workshopId = props.match.params.id;
+  useEffect(() => {
+    getWorkshop(workshopId);
+    getAttendees(workshopId);
+  }, [workshopId]);
 
-    const { workshop, getWorkshop, getAttendees, attendees } = useContext(WorkshopContext);
-    const [displayModal, setDisplayModal] = useState(false);
-    const [workshopInEdit, setWorkshopInEdit] = useState([]);
-    const [modal, setModal] = useState("");
+  const toggleDisplayModal = () => {
+    setDisplayModal(!displayModal);
+    setWorkshopInEdit(workshop);
+    getWorkshop(workshopId);
+  };
 
-    useEffect(() => {
-        getWorkshop(workshopId);
-        getAttendees(workshopId)
-    }, [workshopId]);
+  const selectModal = (modal) => {
+    setModal(modal);
+  };
 
-    const toggleDisplayModal = () => {
-        setDisplayModal(!displayModal)
-        setWorkshopInEdit(workshop)
-        getWorkshop(workshopId)
-    }
-
-    const selectModal = (modal) => {
-        setModal(modal);
-    }
-
-    return ( 
-        workshop !== [] &&
-        <div>
-            {displayModal && <ModalForm workshopInEdit={workshopInEdit} toggleDisplayModal={toggleDisplayModal} active={modal} attendees={attendees}/>}
-             <div className="workshop-attendees-header">
-                <h1>Workshop Info</h1>
-            </div>
-            <div className="workshop-attendees-body">
-                <WorkshopInfo workshop={workshop} toggleDisplayModal={toggleDisplayModal} selectModal={selectModal}/>
-                <AttendeesList attendees={attendees}/>
-            </div>
+  return (
+    workshop !== [] && (
+      <div>
+        {displayModal && (
+          <ModalForm
+            workshopInEdit={workshopInEdit}
+            toggleDisplayModal={toggleDisplayModal}
+            active={modal}
+            attendees={attendees}
+          />
+        )}
+        <div className="workshop-attendees-header">
+          <h1>Workshop Info</h1>
         </div>
-    );
-}
- 
+        <div className="workshop-attendees-body">
+          <WorkshopInfo
+            workshop={workshop}
+            toggleDisplayModal={toggleDisplayModal}
+            selectModal={selectModal}
+            attendees={attendees}
+          />
+          <AttendeesList attendees={attendees} />
+        </div>
+      </div>
+    )
+  );
+};
+
 export default WorkshopAttendees;
