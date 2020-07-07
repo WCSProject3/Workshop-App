547c3a5ba17bb5ca7449007a2b6e0c0d94efe586
Arnaud Duhé <anro010@hotmail.com>
2020-07-07 15:24:13 +0100

    on the way - re using workshopattendee components

--- Front-end/src/Components/Speaker/MyWorkshopAttendees.js
+++ Front-end/src/Components/Speaker/MyWorkshopAttendees.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from 'react';
+import AttendeesList from '../Admin/WorkshopAttendeesSubComponents/AttendeesList';
+import WorkshopInfo from '../Admin/WorkshopAttendeesSubComponents/WorkshopInfo';
+import '../Admin/WorkshopAttendees.scss';
+import { WorkshopContext } from '../../Context/WorkshopContext';
 
-const MyWorkshopAttendees = () => {
-    return(
-        <div>MyWorkshopAttendees</div>
-    )
+
+const MyWorkshopAttendees = ( ) => {
+
+    const speakerId = 1;
+
+    const { workshop, getWorkshop, getAttendees, attendees } = useContext(WorkshopContext);
+
+    useEffect(() => {
+        getWorkshop(speakerId);
+        getAttendees(speakerId)
+    }, [speakerId]);
+
+    return ( 
+        <div>
+            <div className="workshop-attendees-header">
+                <h1>Workshop Info</h1>
+            </div>
+            <div className="workshop-attendees-body" >
+                <WorkshopInfo workshop={workshop}/>
+                <AttendeesList attendees={attendees}/>
+            </div>
+        </div>
+    );
 }
-
-export default MyWorkshopAttendees
+ 
+export default MyWorkshopAttendees;
