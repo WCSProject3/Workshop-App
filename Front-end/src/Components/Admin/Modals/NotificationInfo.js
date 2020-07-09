34c9f99a6907331f2df13e5a8c12b9742e7f25ff
François ARBOGAST <arbogast.francois@gmail.com>
2020-07-09 16:42:37 +0100

    Fixed date, dropdown, delete

--- Front-end/src/Components/Admin/Modals/NotificationInfo.js
+++ Front-end/src/Components/Admin/Modals/NotificationInfo.js
@@ -1,49 +1,37 @@
-import React, { useContext } from 'react';
-import { MdDelete } from 'react-icons/md';
-import { NotificationContext } from '../../../Context/NotificationContext';
+import React, { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import { NotificationContext } from "../../../Context/NotificationContext";
 
+const NotificationInfo = ({ toggleDisplayModal, notification }) => {
+  const { deleteNotification } = useContext(NotificationContext);
 
-const NotificationInfo = ({toggleDisplayModal, notification}) => {
-
-    const { deleteNotification } = useContext(NotificationContext)
-
-    const handleDelete = (id) => {
-      deleteNotification(id)
-      toggleDisplayModal()
-    }
-
-    return(
-        <div className="temp-notification-info">
-        <div className="temp-notification-info-header">
-          <div>{notification.date}</div>
-          <div className="temp-notification-info-header-btns">
-            <button onClick={() => handleDelete(notification.id)}><MdDelete /></button>
-          </div>
+  return (
+    <div className="temp-notification-info">
+      <div className="temp-notification-info-header">
+        <div>{notification.date}</div>
+      </div>
+      <div className="temp-notification-info-body">
+        <div>
+          <span>To:</span> {notification.send_to}
         </div>
-        <div className="temp-notification-info-body">
-          <div>
-            <span>To:</span> {notification.send_to}
-          </div>
-          <div>
-            <span>Subject :</span>
-            {notification.subject}
-          </div>
-          <div>
-            <span>Content: </span>
-            {notification.content}
-          </div>
-          <div>
-            <span>State: </span>
-            {notification.state}
-          </div>
+        <div>
+          <span>Subject :</span>
+          {notification.subject}
         </div>
-        <div className="temp-notification-info-footer">
-          <button onClick={toggleDisplayModal}>
-            close
-          </button>
+        <div>
+          <span>Content: </span>
+          {notification.content}
+        </div>
+        <div>
+          <span>State: </span>
+          {notification.state}
         </div>
       </div>
-    )
-}
+      <div className="temp-notification-info-footer">
+        <button onClick={toggleDisplayModal}>close</button>
+      </div>
+    </div>
+  );
+};
 
-export default NotificationInfo
+export default NotificationInfo;
--- Front-end/src/Components/Admin/WorkshopAttendeesSubComponents/WorkshopInfo.js
+++ Front-end/src/Components/Admin/WorkshopAttendeesSubComponents/WorkshopInfo.js
@@ -8,6 +8,7 @@ import ReactPDF, {
 } from "@react-pdf/renderer";
 import ReactDOM from "react-dom";
 import WorkshopView from "../Documents/WorkshopView";
+import { FaListUl } from "react-icons/fa";
 
 const WorkshopInfo = ({
   workshop,
@@ -15,86 +16,89 @@ const WorkshopInfo = ({
   selectModal,
   attendees,
 }) => {
-  
   //const user = { role_id: 3 }
-  
+
   const openModal = (modal) => {
     toggleDisplayModal();
     selectModal(modal);
   };
 
+  const handleDelete = () => {
+    toggleDisplayModal(
+      "confirm",
+      "are you sure you want to delete this workshop?",
+      workshop.id,
+      workshop.enrolled_attendees
+    );
+  };
+
   return (
-    workshop !==[] &&
-    (<div>
-      <div className="workshop-info">
-        <div className="workshop-info-header">
-          <div className="workshop-info-date">
-            {workshop.date && (
-              <div className="date">{`${workshop.date.substring(8, 10)} ${
-                workshop.workshop_month
-              } - ${workshop.starting_hour.substring(
-                0,
-                5
-              )}-${workshop.ending_hour.substring(0, 5)}`}</div>
-            )}
+    workshop !== [] && (
+      <div>
+        <div className="workshop-info">
+          <div className="workshop-info-header">
+            <div className="workshop-info-date">
+              {workshop.date && (
+                <div className="date">{`${workshop.date.substring(8, 10)} ${
+                  workshop.workshop_month
+                } - ${workshop.starting_hour.substring(
+                  0,
+                  5
+                )}-${workshop.ending_hour.substring(0, 5)}`}</div>
+              )}
+            </div>
+            <div className="dropdown">
+              <button className="options-icon">
+                <FaListUl />
+              </button>
+              <div className="btns-dropdown">
+                <button onClick={() => openModal("notification")}>
+                  Notification
+                </button>
+                <button onClick={() => openModal("workshop")}>Edit</button>
+                <PDFDownloadLink
+                  document={
+                    <WorkshopView workshop={workshop} attendees={attendees} />
+                  }
+                  fileName="Test.pdf"
+                >
+                  {({ blob, url, loading, error }) =>
+                    loading ? <button>loading</button> : <button>export</button>
+                  }
+                </PDFDownloadLink>
+              </div>
+            </div>
           </div>
-          <div className="workshop-info-header-btns">
-            <button
-              className="workshop-icons"
-              onClick={() => openModal("notification")}
-            >
-              <MdMessage />
-            </button>
-            <button
-              className="workshop-icons"
-              onClick={() => openModal("workshop")}
-            >
-              <MdEdit />
-            </button>
-            <button className="workshop-icons">
-              <MdDelete />
-            </button>
-            <PDFDownloadLink
-              document={
-                <WorkshopView workshop={workshop} attendees={attendees} />
-              }
-              fileName="Test.pdf"
-            >
-              {({ blob, url, loading, error }) =>
-                loading ? <button>loading</button> : <button>export</button>
-              }
-            </PDFDownloadLink>
-          </div>
-        </div>
-        <div className="workshop-info-body">
-          <div className="workshop-info-left">
-            <h2>{workshop.title}</h2>
-            <h4>{workshop.workshop_speaker}</h4>
-            <p>{workshop.description}</p>
-          </div>
-          <div className="workshop-info-right">
-            <div className="room-manager">
-              <p className="room">
-                <span>Room:</span> {workshop.room}
+          <div className="workshop-info-body">
+            <div className="workshop-info-left">
+              <h2>{workshop.title}</h2>
+              <h4>{workshop.workshop_speaker}</h4>
+              <p>{workshop.description}</p>
+            </div>
+            <div className="workshop-info-right">
+              <div className="room-manager">
+                <p className="room">
+                  <span>Room:</span> {workshop.room}
+                </p>
+                <p>
+                  <span>Room manager:</span> {workshop.room_manager}
+                </p>
+              </div>
+              <p>
+                <span>Room setup:</span> {workshop.room_type}
               </p>
               <p>
-                <span>Room manager:</span> {workshop.room_manager}
+                <span>Room capacity:</span> {workshop.room_capacity}
+              </p>
+              <p className={workshop.status_open === "1" ? "open" : "closed"}>
+                <span>Registrations:</span>{" "}
+                {workshop.status_open === "1" ? "OPEN" : "CLOSED"}
               </p>
             </div>
-            <p>
-              <span>Room setup:</span> {workshop.room_type}
-            </p>
-            <p>
-              <span>Room capacity:</span> {workshop.room_capacity}
-            </p>
-            <p className={workshop.status_open === "1" ? "open" : "closed"}>
-              <span>Registrations:</span>{" "}
-              {workshop.status_open === "1" ? "OPEN" : "CLOSED"}
-            </p>
           </div>
         </div>
       </div>
-    </div>)
+    )
   );
 };
 //ReactDOM.render(<WorkshopView />, document.getElementById("root"));
